5b2008e336baba4b7aac76ae3e1f9734f38b9d6a
Praveen Kumar <praveenshakya441@gmail.com>
2024-04-27 21:59:56 +0530

    update fileUploader

--- components/shared/fileUploader.tsx
+++ components/shared/fileUploader.tsx
@@ -1,27 +1,24 @@
 'use client'
-
-type FileUploaderProps = {
-    onFieldChange: (url: string) => void
-    imageUrl: string
-    setFiles: Dispatch<SetStateAction<File[]>>
-  }
-
+import Image from 'next/image'
 import { useCallback, Dispatch, SetStateAction } from 'react'
-import  type {FileWithPath}  from '@uploadthing/react'
+// import type { FileWithPath } from '@uploadthing/react'
 import { useDropzone } from '@uploadthing/react/hooks'
 import { generateClientDropzoneAccept } from 'uploadthing/client'
 
 import { Button } from '@/components/ui/button'
 import { convertFileToUrl } from '@/lib/utils'
 
-
+type FileUploaderProps = {
+  onFieldChange: (url: string) => void
+  imageUrl: string
+  setFiles: Dispatch<SetStateAction<File[]>>
+}
 
 export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) {
-    
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
     setFiles(acceptedFiles)
     onFieldChange(convertFileToUrl(acceptedFiles[0]))
-  }, [])
+  }, [onFieldChange, setFiles])
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
@@ -36,7 +33,7 @@ export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploader
 
       {imageUrl ? (
         <div className="flex h-full w-full flex-1 justify-center ">
-          <img
+          <Image
             src={imageUrl}
             alt="image"
             width={250}
@@ -46,7 +43,7 @@ export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploader
         </div>
       ) : (
         <div className="flex-center flex-col py-5 text-grey-500">
-          <img src="/assets/icons/upload.svg" width={77} height={77} alt="file upload" />
+          <Image src="/assets/icons/upload.svg" width={77} height={77} alt="file upload" />
           <h3 className="mb-2 mt-2">Drag photo here</h3>
           <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
           <Button type="button" className="rounded-full">
